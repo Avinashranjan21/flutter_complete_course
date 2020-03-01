2451d2edc170f7db5c519cae81be038b6320a7d5
Avinash Pandey <avinash.ranjan21@gmail.com>
2020-03-01 13:27:14 +0530

    Module_1: multiple choice question done.

---
+++ lib/answer.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final Function selectHandler;
+  final String buttonText;
+
+  Answer(this.buttonText, this.selectHandler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+        color: Colors.blue,
+        textColor: Colors.white,
+        child: Text(
+          buttonText,
+        ),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
